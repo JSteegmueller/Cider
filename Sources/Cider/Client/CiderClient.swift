3720f5bb23a72758f0cb18901b24c864489bea42
Janik Steegmüller <me@jsteegmueller.net>
2019-07-14 15:50:28 +0200

    using quicktype for heavy-rotation

--- Sources/Cider/Client/CiderClient.swift
+++ Sources/Cider/Client/CiderClient.swift
@@ -168,9 +168,9 @@ public struct CiderClient {
      Get heavy-rotation from user
      
      */
-    public func heavyRotation(limit: Int? = nil, offset: Int? = nil, types: [MediaType]? = nil, completion: ((Album?, Error?) -> Void)?) {
+    public func heavyRotation(limit: Int? = nil, offset: Int? = nil, completion: ((HeavyRotationAlbum?, Error?) -> Void)?) {
         let request = urlBuilder.heavyRotationRequest(limit: limit, offset: offset)
-        fetch(request) { (results: ResponseRoot<Album>?, error) in completion?(results?.data?.first, error) }
+        fetch(request) { (results: HeavyRotationAlbum?, error) in completion?(results, error) }
     }
 
 
@@ -193,6 +193,25 @@ public struct CiderClient {
 
     // MARK: Helpers
 
+    private func fetch(_ request: URLRequest, completion: ((HeavyRotationAlbum?, Error?) -> Void)?) {
+        fetcher.fetch(request: request) { (data, error) in
+            guard let data = data else {
+                completion?(nil, error)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let results = try decoder.decode(HeavyRotationAlbum.self, from: data)
+            
+                completion?(results, nil)
+            } catch {
+                completion?(nil, error)
+            }
+        }
+    }
+
+    
     private func fetch<T>(_ request: URLRequest, completion: ((ResponseRoot<T>?, Error?) -> Void)?) {
         fetcher.fetch(request: request) { (data, error) in
             guard let data = data else {
