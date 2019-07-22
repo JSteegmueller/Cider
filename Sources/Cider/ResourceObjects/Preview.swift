230d148d1613a84ae0189d17c1a81ec302e8b4e6
Janik Steegmüller <me@jsteegmueller.net>
2019-07-22 10:21:53 +0200

    add preview-attr. to songs

---
+++ Sources/Cider/ResourceObjects/Preview.swift
@@ -0,0 +1,14 @@
+//
+//  Preview.swift
+//  Cider
+//
+//  Created by Janik Steegmüller on 22.07.19.
+//  Copyright © 2019 Scott Hoyt. All rights reserved.
+//
+
+import Foundation
+
+public struct Preview: Codable {
+    public let artwork: Artwork?
+    public let url: String
+}
